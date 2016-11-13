3d6b2c192e0a68cfc77201086837ddf2365607c6
Zhousenye Liu <jooppp@live.com>
2016-11-13 17:36:05 -0500

    Update RAIDEN.rb

--- RAIDEN.rb
+++ RAIDEN.rb
@@ -12,6 +12,8 @@ class RAIDEN < Gosu::Window
 	def update
 		@player.turn_left if button_down?(Gosu::KbLeft)
 		@player.turn_right if button_down?(Gosu::KbRight)
+		@player.accelerate if button_down?(Gosu::KbUp)
+		@player.move
 	end
 	
 end
