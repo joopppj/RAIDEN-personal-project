88ecf2aced84ff9e43e4590e4d5d1d73b9212dcc
Zhousenye Liu <jooppp@live.com>
2016-11-13 16:57:32 -0500

    Now we can turn the ship

---
+++ RAIDEN.rb
@@ -0,0 +1,20 @@
+require 'gosu'
+require_relative 'player'
+class RAIDEN < Gosu::Window
+	def initialize
+		super(800,600)
+		self.caption = 'RAIDEN'
+		@player = Player.new(self)
+	end
+	def draw
+		@player.draw
+	end
+	def update
+		@player.turn_left if button_down?(Gosu::KbLeft)
+		@player.turn_right if button_down?(Gosu::KbRight)
+	end
+	
+end
+
+window = RAIDEN.new
+window.show
