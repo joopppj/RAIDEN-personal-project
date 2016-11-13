5105308cb5547147c882cba837aebfee92740a31
Zhousenye Liu <jooppp@live.com>
2016-11-13 17:37:02 -0500

    Update player.rb

--- player.rb
+++ player.rb
@@ -4,6 +4,8 @@ class Player
 		@y=200
 		@angle =0
 		@image = Gosu::Image.new('images/player.png')
+		@velocity_x=0
+		@velocity_y=0
 	end
 	def draw
 	@image.draw_rot(@x,@y,1,@angle)
@@ -14,4 +16,15 @@ class Player
 	def turn_left
 		@angle -=3
 	end
+	def accelerate 
+		@velocity_x += Gosu.offset_x(@angle,1)
+		@velocity_y += Gosu.offset_y(@angle,1)
+	end
+	def move
+		@x +=@velocity_x
+		@y +=@velocity_y
+		@velocity_x *=0.9
+		@velocity_x *=0.9
+	end
+
 end
