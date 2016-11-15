0254f4b23f549689eecbf3be20242ad14bb6832b
Zhousenye Liu <jooppp@live.com>
2016-11-14 22:40:06 -0500

    update RAIDEN.rb so that it can show the enemy

--- RAIDEN.rb
+++ RAIDEN.rb
@@ -1,21 +1,26 @@
 require 'gosu'
 require_relative 'player'
+require_relative 'enemy'
 class RAIDEN < Gosu::Window
+	WIDTH=800
+	HEIGHT=600
 	def initialize
-		super(800,600)
+		super(WIDTH,HEIGHT)
 		self.caption = 'RAIDEN'
 		@player = Player.new(self)
+		@enemy=Enemy.new(self)
 	end
 	def draw
 		@player.draw
+		@enemy.draw
 	end
 	def update
 		@player.turn_left if button_down?(Gosu::KbLeft)
 		@player.turn_right if button_down?(Gosu::KbRight)
 		@player.accelerate if button_down?(Gosu::KbUp)
 		@player.move
+		@enemy.move
 	end
-	
 end
 
 window = RAIDEN.new
