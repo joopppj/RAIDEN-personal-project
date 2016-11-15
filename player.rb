4436ea8b77f13245f8aa21856e216525e67c2e63
Zhousenye Liu <jooppp@live.com>
2016-11-14 22:39:03 -0500

    add bound so that player and not fly out of window

--- player.rb
+++ player.rb
@@ -1,4 +1,7 @@
 class Player
+	ROTATION_SPEED=2 
+	ACCELERATION=2
+	FRICTION=0.9
 	def initialize(window)
 		@x=200
 		@y=200
@@ -6,25 +9,42 @@ class Player
 		@image = Gosu::Image.new('images/player.png')
 		@velocity_x=0
 		@velocity_y=0
+		@window=window
+		@radius=0
 	end
 	def draw
 	@image.draw_rot(@x,@y,1,@angle)
 	end
 	def turn_right
-		@angle +=3
+		@angle +=ROTATION_SPEED
 	end
 	def turn_left
-		@angle -=3
+		@angle -=ROTATION_SPEED
 	end
 	def accelerate 
-		@velocity_x += Gosu.offset_x(@angle,1)
-		@velocity_y += Gosu.offset_y(@angle,1)
+		@velocity_x += Gosu.offset_x(@angle,ACCELERATION)
+		@velocity_y += Gosu.offset_y(@angle,ACCELERATION)
 	end
 	def move
 		@x +=@velocity_x
 		@y +=@velocity_y
-		@velocity_x *=0.9
-		@velocity_x *=0.9
+		@velocity_x *=FRICTION
+		@velocity_x *=FRICTION
+		if @x > @window.width - @radius
+       		@velocity_x = 0
+       		@x = @window.width - @radius
+    	end
+    	if @x < @radius
+      		@velocity_x = 0
+      		@x = @radius
+    	end
+    	 if @y > @window.height - @radius
+      		@velocity_y = 0
+     	    @y = @window.height - @radius
+    	 end
+		 if @y < @radius
+      		@velocity_y = 0
+      		@y = @radius
+    	end
 	end
-
 end
