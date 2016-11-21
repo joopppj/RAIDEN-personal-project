ef0f0332d828e34343df65aaa0cd075d8d35dd1a
Zhousenye Liu <jooppp@live.com>
2016-11-20 23:54:48 -0500

    add many enemies and control frequency enemy shows

--- RAIDEN.rb
+++ RAIDEN.rb
@@ -4,22 +4,30 @@ require_relative 'enemy'
 class RAIDEN < Gosu::Window
 	WIDTH=800
 	HEIGHT=600
+	ENEMY_FREQUENCY=0.9
 	def initialize
 		super(WIDTH,HEIGHT)
 		self.caption = 'RAIDEN'
 		@player = Player.new(self)
-		@enemy=Enemy.new(self)
+		@enemies=[]
 	end
 	def draw
 		@player.draw
-		@enemy.draw
+		@enemies.each do |enemy|
+			enemy.draw
+		end
 	end
 	def update
 		@player.turn_left if button_down?(Gosu::KbLeft)
 		@player.turn_right if button_down?(Gosu::KbRight)
 		@player.accelerate if button_down?(Gosu::KbUp)
 		@player.move
-		@enemy.move
+		if rand<ENEMY_FREQUENCY
+			@enemies.push Enemy.new(self)
+		end
+		@enemies.each do |enemy|
+			enemy.move
+		end
 	end
 end
 
