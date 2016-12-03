ac17e7ccdfcdedaa50de5e7e35f8f33be3dec311
Zhousenye Liu <jooppp@live.com>
2016-12-03 02:54:07 -0500

    Add files via upload

---
+++ RAIDEN.rb
@@ -0,0 +1,58 @@
+require 'gosu'
+require_relative 'player'
+require_relative 'enemy'
+require_relative 'shoot'
+class RAIDEN < Gosu::Window
+	 WIDTH = 800
+  HEIGHT = 600
+  ENEMY_FREQUENCY = 0.05
+
+	def initialize
+		super(WIDTH,HEIGHT)
+		self.caption = 'RAIDEN'
+		@player = Player.new(self)
+		@enemies=[]
+		@shoots=[]
+	end
+	def update
+		@player.turn_left if button_down?(Gosu::KbLeft)
+		@player.turn_right if button_down?(Gosu::KbRight)
+		@player.accelerate if button_down?(Gosu::KbUp)
+		@player.move
+		if rand<ENEMY_FREQUENCY
+			@enemies.push Enemy.new(self)
+		end
+		@enemies.each do |zz|
+			zz.move
+		end
+		@shoots.each do |shoot|
+			shoot.move
+		end
+		@enemies.each do |enemy|
+      			@shoots.each do |bullet|
+        				distance = Gosu.distance(enemy.getx, enemy.gety, bullet.x, bullet.y)
+        				if distance < enemy.radius + bullet.radius
+          				@enemies.delete enemy
+          					@shoots.delete bullet           
+        					end
+     			 end
+   		 end 
+	def button_down(id)
+		if id==Gosu::KbSpace
+			@shoots.push Shoot.new(self, @player.x, @player.y,@player.angle)
+		end
+	end
+	def draw
+		@player.draw
+		@enemies.each do |enemy|
+			enemy.draw
+		end
+		@shoots.each do |shoot|
+			shoot.draw
+		end
+	
+	end
+end
+
+window = RAIDEN.new
+window.show end
---
+++ enemy.rb
@@ -0,0 +1,24 @@
+class Enemy
+	SPEED = 4
+	def initialize(window)
+		@radius=20
+		@x= rand(window.width-2*@radius)+@radius
+		@y=40
+		@image=Gosu::Image.new('images/enemy.png')
+	end
+	def draw
+    @image.draw(@x - @radius, @y - @radius, 1)
+  end
+  def move
+    @y += SPEED
+  end
+  def getx
+  @x
+  end
+  def gety
+  @y
+  end
+  def radius
+  @radius
+  end
+end
