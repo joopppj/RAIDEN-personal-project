4211623e7151b71e01958bff144cc70e023c2393
Zhousenye Liu <jooppp@live.com>
2016-12-28 22:36:40 -0500

    Update shoot.rb

--- shoot.rb
+++ shoot.rb
@@ -16,4 +16,11 @@ end
 def draw
   @image.draw(@x - @radius, @y - @radius, 1)
 end
+def onscreen?
+    right =@window.width+@radius
+    left= -@radius
+    top= -@radius
+    bottom = @window.height + @radius
+    @x>left and @x < right and @y > top and @y<bottom
+end
 end
