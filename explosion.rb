28152c922c2b10a3ffd614d55bb864a90d3dc844
Zhousenye Liu <jooppp@live.com>
2016-12-28 22:38:18 -0500

    Update explosion.rb

--- explosion.rb
+++ explosion.rb
@@ -1,4 +1,5 @@
 class Explosion
+    
 	def initialize(window,x,y)
 		@x=x
 		@y=y
@@ -6,7 +7,11 @@ class Explosion
 		@images= Gosu::Image.load_tiles('images/explosions.png',60,60)
 		@image_index=0
 		@finished=false
+        
 	end
+    def getf
+        @finished
+    end
 	def draw
 		if @image_index<@images.count
 			@images[@image_index].draw(@x-@radius,@y-@radius,2)
